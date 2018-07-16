7ccb4c8df336d568c3c06d003e43bc767eb27ade
Ryan Hoegg <ryan.hoegg@gmail.com>
2018-07-16 16:42:05 -0700

    ignore old test for walkthrough 3-1

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,15 +1,14 @@
 package com.mulesoft.training;
 
-import static org.junit.Assert.*;
-
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven");
+    @Test @Ignore
+    public void mavenFlowReturnsFlights() throws Exception {
+        runFlowAndExpect("mavenFlow", "[[{\"DESTINATION\":\"SFO\",\"PRICE\":555,\"ORIGIN\":\"YYZ\",\"ID\":0},{\"DESTINATION\":\"LAX\",\"PRICE\":450,\"ORIGIN\":\"YYZ\",\"ID\":1},{\"DESTINATION\":\"SEA\",\"PRICE\":777,\"ORIGIN\":\"SQL\",\"ID\":2},{\"DESTINATION\":\"SFO\",\"PRICE\":999,\"ORIGIN\":\"SQL\",\"ID\":3}]]");
     }
     
     @Override
