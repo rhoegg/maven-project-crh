6f4946c08eb3fe9375215f7eb6808d5fc455b8a8
Ryan Hoegg <ryan.hoegg@gmail.com>
2018-07-17 09:41:54 -0700

    Added Content Type header validation test.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,14 +1,24 @@
 package com.mulesoft.training;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
     @Test @Ignore
     public void mavenFlowReturnsFlights() throws Exception {
-        runFlowAndExpect("mavenFlow", "[[{\"DESTINATION\":\"SFO\",\"PRICE\":555,\"ORIGIN\":\"YYZ\",\"ID\":0},{\"DESTINATION\":\"LAX\",\"PRICE\":450,\"ORIGIN\":\"YYZ\",\"ID\":1},{\"DESTINATION\":\"SEA\",\"PRICE\":777,\"ORIGIN\":\"SQL\",\"ID\":2},{\"DESTINATION\":\"SFO\",\"PRICE\":999,\"ORIGIN\":\"SQL\",\"ID\":3}]]");
+        runFlowAndExpect("retrieveFlights", "[[{\"DESTINATION\":\"SFO\",\"PRICE\":555,\"ORIGIN\":\"YYZ\",\"ID\":0},{\"DESTINATION\":\"LAX\",\"PRICE\":450,\"ORIGIN\":\"YYZ\",\"ID\":1},{\"DESTINATION\":\"SEA\",\"PRICE\":777,\"ORIGIN\":\"SQL\",\"ID\":2},{\"DESTINATION\":\"SFO\",\"PRICE\":999,\"ORIGIN\":\"SQL\",\"ID\":3}]]");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Override
